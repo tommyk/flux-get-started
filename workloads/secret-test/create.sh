9e05e07c4529f66d0861178e543bc1353275a06b
tommy kelly <tommy.kelly@awarehq.com>
2019-10-01 18:35:11 -0400

    adding double quotes

--- workloads/secret-test/create.sh
+++ workloads/secret-test/create.sh
@@ -5,7 +5,7 @@ metadata:
   name: secret-from-flux-manifest
 type: Opaque
 data:
-  config4.yaml: $(cat fake.yaml | base64 -w 0)
-  config3.yaml: $(cat fake.yaml | base64 -w 0)
+  config4.yaml: "$(cat fake.yaml | base64 -w 0)"
+  config3.yaml: "$(cat fake.yaml | base64 -w 0)"
 EOF
 )"

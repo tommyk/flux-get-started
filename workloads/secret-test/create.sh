bd54f5a450c266416514a4ddd1d1dd1ccb0334d0
tommy kelly <tommy.kelly@awarehq.com>
2019-10-01 18:31:57 -0400

    tset

--- workloads/secret-test/create.sh
+++ workloads/secret-test/create.sh
@@ -5,7 +5,7 @@ metadata:
   name: secret-from-flux-manifest
 type: Opaque
 data:
-  config.yaml: '$(cat fake.yaml | base64 -w 0)'
-  config2.yaml: '$(cat fake.yaml | base64 -w 0)'
+  config4.yaml: $(cat fake.yaml | base64 -w 0)
+  config3.yaml: $(cat fake.yaml | base64 -w 0)
 EOF
 )"
